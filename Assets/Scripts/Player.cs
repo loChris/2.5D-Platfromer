d4485758424c5805695aac2d1a0521bbf605df9c
loChris <44450518+loChris@users.noreply.github.com>
2020-03-03 10:30:53 -0500

    added horizontal movement to the player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+    private float _speed = 10f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get horizontal input
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //define direction based on that input
+        Vector3 direction = new Vector3(horizontalInput,0, 0);
+
+        //move based that direction
+        transform.Translate(direction * (_speed * Time.deltaTime));
+    }
+}
