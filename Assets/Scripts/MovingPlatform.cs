8e07c6173051c1e34fe4481eea61ca94acc28704
loChris <44450518+loChris@users.noreply.github.com>
2020-03-09 19:17:45 -0500

    added a moving platform

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private Transform _pointA, _pointB;
+    [SerializeField] private float _speed = 1f;
+
+    private bool _movingPlatformSwitch = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_movingPlatformSwitch == false)
+        {
+            transform.position = Vector3.MoveTowards(
+                transform.position, 
+                _pointB.position, 
+                _speed * Time.deltaTime
+            );
+        } 
+        else if (_movingPlatformSwitch)
+        {
+            {
+                transform.position = Vector3.MoveTowards(
+                    transform.position,
+                    _pointA.position,
+                    _speed * Time.deltaTime
+                );
+            }
+        }
+
+        if (transform.position == _pointA.position)
+        {
+            _movingPlatformSwitch = false;
+        }
+        else if (transform.position == _pointB.position)
+        {
+            _movingPlatformSwitch = true;
+        }
+    }
+}
